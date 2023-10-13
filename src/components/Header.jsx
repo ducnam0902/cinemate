34af0c6f954173b902cfafd6f6362b3a949ce602
nam <ducnam0902@gmail.com>
2023-10-13 13:37:57 +0000

    create movie list comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import { useEffect, useState } from "react";
 export const Header = () => {
+  const [hidden, setHidden] = useState(false);
+  const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || true);
+
+  const activeClass = "text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
+  const inActiveClass = "text-base block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
     <header>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900 dark:border-b-1">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/" className="flex items-center">
             <img src={Logo} className="h-8 mr-3" alt="Cinemate Logo" />
@@ -11,7 +27,15 @@ export const Header = () => {
               Flowbite
             </span>
           </Link>
-          <div className="flex md:order-2">
+
+          <div id="mobile-nav" className="flex md:order-2">
+            <button
+              type="button"
+              data-tooltip-target="navbarr-search-example-toggle-darkmode-tooltip"
+              onClick={() => setDarkMode(!darkMode)}
+            >
+              darkmode
+            </button>
             <button
               type="button"
               data-collapse-toggle="navbar-search"
@@ -60,6 +84,7 @@ export const Header = () => {
                 id="search-navbar"
                 className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
+                autoComplete="off"
               />
             </div>
             <button
@@ -68,6 +93,7 @@ export const Header = () => {
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-search"
               aria-expanded="false"
+              onClick={() => setHidden(!hidden)}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -87,9 +113,10 @@ export const Header = () => {
               </svg>
             </button>
           </div>
+
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-search"
+            id="nav-links"
+            className={`${hidden ? "hidden" : "empty"} items-center justify-between w-full md:flex md:w-auto md:order-1`}
           >
             <div className="relative mt-3 md:hidden">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -114,13 +141,14 @@ export const Header = () => {
                 id="search-navbar"
                 className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
+                autoComplete="off"
               />
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
                   to="/"
-                  className="text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className={({ isActive }) => isActive ? activeClass : inActiveClass}
                   aria-current="page"
                   end
                 >
@@ -130,24 +158,22 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/movies/popular"
-                  className="text-base block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={({ isActive }) => isActive ? activeClass : inActiveClass}
                 >
-                  Popular 
+                  Popular
                 </NavLink>
               </li>
               <li>
                 <NavLink
                   to="/movies/top"
-                  className="text-base block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
+                  className={({ isActive }) => isActive ? activeClass : inActiveClass}>
                   Top Rated
                 </NavLink>
               </li>
               <li>
                 <NavLink
                   to="/movies/upcoming"
-                  className="text-base block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
+                  className={({ isActive }) => isActive ? activeClass : inActiveClass}>
                   Upcoming
                 </NavLink>
               </li>
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"
+import { Card } from "../components/Card"
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch();
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <main>
-      <div className="text-9xl">
-        MovieList
-      </div>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {
+            movies.map((movie) => (
+              <Card key={movie.id} movie={movie} />
+            ))
+          }
+        </div>
+      </section>
     </main>
   )
 }
